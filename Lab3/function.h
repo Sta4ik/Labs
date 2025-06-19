e441e2451ed7142e31c2d039e35f26f9cbc0b488
Sta4ik <ctac354.354@gmail.com>
2025-06-19 11:33:35 +0300

    add: lab3 and README

---
+++ Lab3/function.h
@@ -0,0 +1,19 @@
+#include <iostream>
+void AllocationMemory(int64_t, double*&);
+double* FillArray(int64_t, double*);
+void DeleteArray(double*&);
+size_t InputSize();
+double FindMaxElement(int64_t, double*);
+double FindSumBetweenPositive(int64_t, double*);
+double Positive(int64_t, double*);
+int64_t FindMinElementIndex(int64_t, double*);
+double FindSumElementArray(int64_t, double*);
+void BubleSort(int64_t, double*);
+void SortEvenAndOdd(int64_t, double*);
+int64_t FindNewSize(int64_t, double*);
+double* SortByCounts(int64_t, double*);
+void Sort(int64_t, int64_t*, double*);
+double* ExpationArray(int64_t, int64_t, int64_t*, double*);
+double* FillArrayRandom(int64_t, double*);
+int32_t InputInputType();
+void PrintArray(int64_t, double*);
